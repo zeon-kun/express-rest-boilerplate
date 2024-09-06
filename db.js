fb34c60c2541679664dfc7ca02b999cfb24118bf
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-09-07 02:14:26 +0700

    changing to compose

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: "mysql",
+  port: process.env.DB_PORT,
   logging: false, // Disable SQL query logging, enable if needed
 });
 
